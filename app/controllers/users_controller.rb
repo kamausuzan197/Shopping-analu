5b814a523a7aed6d2b95e7f91b7990ca6cbe8702
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-10-25 15:43:33 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
     # handles create and autologin
     skip_before_action :authorize, only: :create
+
+
+    def index
+      users=User.all
+      render json:users
+    end
+
+    def show
+      user = User.find_by(id: session[:user_id])
+      if user
+      render json: user
+      else
+          render json: { error: "Not authorized" }, status: :unauthorized
+      end
+  end
+
+
  
     def create
       user = User.create!(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  resources :users
 end
